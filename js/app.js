3bfc57d544471ca2756d12f371b9e1cb09ea62fe
kripakov <olga.orlova.gt.96@gmail.com>
2017-02-15 21:41:45 +0400

    end

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 	$("#toggle").click(function() {
 		$(this).toggleClass("on");
-		$(".menu-author").slideToggle();
+		//$(".menu-author").slideToggle();
+		
+		$(".menu-author").slideToggle(function(){
+			$(".container.navigator .header a").animate({"margin-top":"85px", "margin-left":"25px"});
+		});
+		/*
+		$('#toggle').toggle(function () {
+		    $(".menu-author").css({"margin-top":"48px", "margin-left":"50px"});
+		}, function () {
+		    $(".menu-author").css({"margin-top":"5px", "margin-left":"25px"});
+		});*/
 		return false;
 	});
 });
